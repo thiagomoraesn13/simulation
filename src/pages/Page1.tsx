953bdf324d7636ca31f81d2df99bbdadf37a08c4
Thiago Moraes <tmr@icomp.ufam.edu.br>
2026-02-20 10:25:45 -0400

    add console env

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -2,11 +2,18 @@ import { GenericButton } from "@thiagomoraesn13/ui";
 import { useSimulation } from "../simulation-context";
 import { SectionTitle } from "../SectionTitle";
 
+export const CI_ENV_CHECK = {
+  url: import.meta.env.VITE_ACCOUNTS_API_URL,
+  id: import.meta.env.VITE_API_CLIENT_ID,
+};
+
 export function Page1() {
   const { goToOnboarding, assetsBaseUrl, theme } = useSimulation();
   const logoUrl = `${assetsBaseUrl}/${theme}/logo.png`;
 
   console.log(import.meta.env);
+
+  console.log("CI_ENV_CHECK", CI_ENV_CHECK);
 
   return (
     <div className="p-6 space-y-4">
